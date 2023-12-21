61c891ddd3e2326a69150ed87d5f0b40eacd5784
Jaime Jimenez <jaimejim@gmail.com>
2023-12-21 18:37:41 +0100

    added ollama's llms

--- README.md
+++ README.md
@@ -1,37 +1,49 @@
 # URL Summarizer
 
-This script takes a list of URLs from a text file, scrapes the content of each website, and uses the OpenAI GPT-4 API to generate a summary of each website. The summaries are saved in a Markdown file.
+This script takes a list of URLs from a text file, scrapes the content of each website, and uses OpenAI's 'gpt-4-1106-preview' or a local model to generate a summary of each website. The summaries are saved in a Markdown file.
 
-If you find this useful consider buying me a beer 🍺 or something.
+For local LLMs [Ollama](https://ollama.ai/library/phi) and the selected model must be installed and running. You can find models in the [Ollama Library](https://ollama.ai/library).
 
 ## Prerequisites
+
+- Python env
 
 You can install the necessary Python packages with pip:
 
 ```bash
-pip install requirements.txt
+pip install -r requirements.txt
 ```
 
-You also need to set your OpenAI API key as an environment variable:
+You also need to set your `.env` OpenAI API key as an environment variable:
 
-```bash
-export OPENAI_API_KEY=your-api-key
+```sh
+OPENAI_API_KEY=your-api-key-without-quotes
 ```
-
-Replace your-api-key with your actual OpenAI API key.
 
 ## Usage
 
 Activate the environment (I use fish shell)
 
-```bash
+```sh
 source env/bin/activate.fish
 ```
 
-To run the script, pass the input file (a text file with one URL per line) and the output file (the Markdown file to write the summaries to) as arguments:
+To run the script, pass the input file (a text file with one URL per line) and the output file (the Markdown file to write the summaries to) as arguments. You can also specify the model to use for summarization with the -m or --model option. The default model is 'openai'.
 
-```bash
-python summarizer.py urls.txt output.md
+```sh
+python summarizer.py urls.txt output.md -m model
 ```
 
+Where:
+
+- `input_file` is a text file containing one URL per line.
+- `output_file` is the file where the summarized content will be written.
+- `model` is the model to use for summarization. It can be either 'openai' or the name of a model running locally with an ollama api (e.g., 'phi', 'mixtral'...). If not provided, 'openai' is used by default.
+
 This will create a Markdown file named output.md with the summaries of the websites listed in `urls.txt`.
+
+## Notes on the Local Models
+
+**phi-2** does not seem very well-suited for text summarization but rather simple, short chats. It tends to generate very inaccurate and often non-sensical summaries. It is particularly bad with non-english texts. I do not recommend it other than for testing purposes.
+
+**mixtral** output is very good quality but impossibly slow on my 2,3 GHz Quad-Core Intel Core i7.
--- summarizer.py
+++ summarizer.py
@@ -1,23 +1,28 @@
 import os
 import sys
 import requests
+import argparse
 from bs4 import BeautifulSoup
 from openai import OpenAI, OpenAIError
-import tiktoken
+import json
 from datetime import datetime
 from termcolor import colored
 from itertools import cycle
 
-MAX_TOKENS = 128000  # The new maximum number of tokens for GPT-4 Turbo
+MODEL_MAX_TOKENS = {
+    "openai": 128000,
+    "mistral": 8000,
+    "mixtral": 8000,
+    "phi": 2500
+}
 
-# Initialize OpenAI
 OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 def print_with_timestamp(message, color=None):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(colored(f'[{timestamp}] {message}', color))
 
-def summarize_url(url, color):
+def summarize_url_with_openai(url, color, model):
     print_with_timestamp(f"Processing URL: {url}", color)
     print_with_timestamp("Sending request to URL...", color)
     try:
@@ -28,19 +33,18 @@ def summarize_url(url, color):
     print_with_timestamp("Response received. Parsing HTML...", color)
     soup = BeautifulSoup(response.text, 'html.parser')
     text = soup.get_text()
-
-    # Check if the text exceeds the token limit
-    if len(text) <= MAX_TOKENS:
-        # Process as one chunk
+    
+    max_tokens = MODEL_MAX_TOKENS[model]
+    if len(text) <= max_tokens:
         chunks = [text]
     else:
-        # Split the text into appropriately sized chunks
-        chunks = [text[i:i + MAX_TOKENS] for i in range(0, len(text), MAX_TOKENS)]
+        chunks = [text[i:i + max_tokens] for i in range(0, len(text), max_tokens)]
 
     client = OpenAI()
     summary = ""
     for chunk in chunks:
-        print_with_timestamp("Sending chunk to OpenAI for processing...", color)
+        print_with_timestamp(f"Sending chunk to OpenAI model for processing (chunk length: {len(chunk)} characters)...", color)
+        print_with_timestamp(f"Text being sent for summarizing: {chunk[-100:]}", color)
         try:
             response = client.chat.completions.create(
                 model="gpt-4-1106-preview",
@@ -59,7 +63,60 @@ def summarize_url(url, color):
 
     return summary
 
-def main(input_file, output_file):
+
+def summarize_url_with_ollama(url, color, model):
+    print_with_timestamp(f"Processing URL: {url} with {model} model", color)
+    print_with_timestamp("Sending request to URL...", color)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.SSLError:
+        print_with_timestamp("SSL Verification failed. Retrying without SSL Verification...", 'yellow')
+        response = requests.get(url, verify=False)
+    print_with_timestamp("Response received. Parsing HTML...", color)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.get_text()
+
+    max_tokens = MODEL_MAX_TOKENS[model]
+    if len(text) <= max_tokens:
+        chunks = [text]
+    else:
+        chunks = [text[i:i + max_tokens] for i in range(0, len(text), max_tokens)]
+
+    summary = ""
+    for chunk in chunks:
+        print_with_timestamp(f"Sending chunk to {model} model for processing (chunk length: {len(chunk)} characters)...", color)
+        print_with_timestamp(f"Text being sent for summarizing: {chunk[-100:]}", color)
+        try:
+            prompt = f"Generate a markdown formatted summary of the main aspects of this website, with features and useful information. Include a title in '##' format and embed links where necessary. The output should be a maximum 2 paragraphs of text without subsections, but including embedded links where necessary: {chunk}"
+            response = requests.post('http://localhost:11434/api/generate', data=json.dumps({
+                "model": model,
+                "prompt": prompt,
+                "stream": False
+            }), headers={'Content-Type': 'application/json'})
+            response_json = response.json()
+            if 'response' in response_json:
+                summary += response_json['response'] + "\n\n"
+        except Exception as e:
+            print_with_timestamp(f"An error occurred: {str(e)}", 'red')
+
+    # Summarize the final summary
+    print_with_timestamp("Summarizing the final summary...", color)
+    try:
+        prompt = f"Generate a markdown formatted summary of the main aspects of this website, with features and useful information. Include a title in '##' format and embed links where necessary. The output should be a maximum 2 paragraphs of text without subsections, but including embedded links where necessary: {summary}"
+        response = requests.post('http://localhost:11434/api/generate', data=json.dumps({
+            "model": model,
+            "prompt": prompt,
+            "stream": False
+        }), headers={'Content-Type': 'application/json'})
+        response_json = response.json()
+        if 'response' in response_json:
+            summary = response_json['response']
+    except Exception as e:
+        print_with_timestamp(f"An error occurred while summarizing the final summary: {str(e)}", 'red')
+
+    return summary
+
+def main(input_file, output_file, model):
     print_with_timestamp(f"Reading URLs from input file: {input_file}", 'green')
     with open(input_file, 'r') as f:
         urls = [line.strip() for line in f if line.strip()]
@@ -68,10 +125,18 @@ def main(input_file, output_file):
     colors = cycle(['blue', 'magenta', 'cyan', 'yellow', 'white'])
     for url in urls:
         color = next(colors)
-        summary = summarize_url(url, color)
+        if model == 'openai':
+            summary = summarize_url_with_openai(url, color)
+        else:
+            summary = summarize_url_with_ollama(url, color, model)
         with open(output_file, 'a') as f:
             f.write(f'\n{summary}- {url}\n')
         print_with_timestamp(f"Appended summary to {output_file}", 'green')
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', help='The input file containing URLs')
+    parser.add_argument('output_file', help='The output file to write summaries')
+    parser.add_argument('-m', '--model', help='The model to use for summarization (openai or one ollama)', default='openai')
+    args = parser.parse_args()
+    main(args.input_file, args.output_file, args.model)
